f24700f795ec8a6127b5ee211e3e609c89b36632
Akash NP <akashnp1998@gmail.com>
2022-10-31 20:23:17 +0530

    Setup Card component (FE) to consume FastAPI endpoint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,32 @@
 import Card from './components/Card';
+import axios from 'axios';
+import useSWR from 'swr';
+
+async function dataFetcher(url) {
+  return await axios.get(url).then(res => res.data.data[0])
+}
 
 function App() {
+
+  const {data, setData} = useSWR("http://127.0.0.1:8000/polo/", dataFetcher)
+  console.log(data)
+
   return (
     <div className='mx-20'>
       <h1 className='text-3xl bold py-5'>Find My Polo</h1>
       <div className='grid grid-cols-3 gap-5'>
-        <Card/>
+        {data.map((listing) => {
+          return (
+            <Card
+              key={listing._id}
+              adTitle={listing.ad_title}
+              askingPrice={listing.asking_price}
+              modelYear={listing.model_year}
+              kmsDriven={listing.kms_driven}
+              adLocation={listing.ad_location}            
+            />
+          )
+        })}
       </div>
     </div>
   );
--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,25 +1,25 @@
 
-export default function Card() {
+export default function Card({ adTitle, askingPrice, modelYear, kmsDriven, adLocation }) {
     return (
         <div className="bg-slate-100 drop-shadow-md p-8">
             <h1 className="text-xl font-bold">
-                Volkswagen Polo GT TSI, 2014, Petrol
+                {adTitle}
             </h1>
             <div className='grid grid-cols-2 gap-2 py-5'>
                 <div className='p-1 rounded flex'>
-                    <p className='bg-slate-700 rounded-l  p-2 w-full text-center text-white'>20,000 KM</p>
+                    <p className='bg-slate-700 rounded-l  p-2 w-full text-center text-white'>{kmsDriven}</p>
                     <p className='bg-slate-200 rounded-r  p-2'>5.0</p>
                 </div>
                 <div className='p-1 rounded flex'>
-                    <p className='bg-slate-700 rounded-l  p-2 w-full text-center text-white'>₹ 7,20,000</p>
+                    <p className='bg-slate-700 rounded-l  p-2 w-full text-center text-white'>{askingPrice}</p>
                     <p className='bg-slate-200 rounded-r  p-2'>5.0</p>
                 </div>
                 <div className='p-1 rounded flex'>
-                    <p className='bg-slate-700 rounded-l  p-2 w-full text-center text-white'>2022</p>
+                    <p className='bg-slate-700 rounded-l  p-2 w-full text-center text-white'>{modelYear}</p>
                     <p className='bg-slate-200 rounded-r  p-2'>5.0</p>
                 </div>
                 <div className='p-1 rounded flex'>
-                    <p className='bg-slate-700 rounded-l  p-2 w-full text-center text-white'>Ottapalam</p>
+                    <p className='bg-slate-700 rounded-l  p-2 w-full text-center text-white'>{adLocation}</p>
                     <p className='bg-slate-200 rounded-r  p-2'>5.0</p>
                 </div>
             </div>
