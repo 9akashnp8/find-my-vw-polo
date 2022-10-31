451e33df90d5d3eace045b58679f156574044128
Akash NP <akashnp1998@gmail.com>
2022-10-31 16:35:10 +0530

    Update CORS to allow API requests from different origin (Frontend)

--- server/app.py
+++ server/app.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from server.routes.polo import router as PoloAPIRouter
 
 app = FastAPI()
+
+origins = ['https://localhost:3000']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methos=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(PoloAPIRouter, tags=["polo"], prefix="/polo")
 
 @app.get("/", tags=["home"])
 def read_root():
     return {'message':'Welcome to this app'}
-
-# Get OLX Listings
-
-# Get OLX Listing by ID
