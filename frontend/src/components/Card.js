6eaa1b3e686a3633ade4068a03c74e553038abc6
Akash NP <akashnp1998@gmail.com>
2022-10-31 14:33:50 +0530

    Intialize React Front end with basic card
    The react front end will be used to display the API data along with other
    features as sorting by custom rating.

---
+++ frontend/src/components/Card.js
@@ -0,0 +1,21 @@
+
+
+export default function Card() {
+    return (
+        <>
+            <h1>Volkswagen Polo GT TSI, 2014, Petrol</h1>
+            <div>
+                <p>20,000KM [5.0]</p>
+            </div>
+            <div>
+                <p>7,40,000 [3.0]</p>
+            </div>
+            <div>
+                <p>2022 [5.0]</p>
+            </div>
+            <div>
+                <a href='https://www.olx.in/item/volkswagen-polo-gt-tsi-2014-petrol-iid-1689682101'>Go to ad</a>
+            </div>
+        </>
+    )
+}
